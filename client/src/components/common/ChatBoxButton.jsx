651476f6b7b4bda02c48f8349dc37e50ef433186
Prakash Kumar <prakashyadav9576@gmail.com>
2025-12-14 19:29:46 +0530

    css update

--- client/src/components/common/ChatBoxButton.jsx
+++ client/src/components/common/ChatBoxButton.jsx
@@ -4,9 +4,9 @@ function ChatBoxButton({ fn }) {
   return (
     <button
       onClick={fn}
-      className="h-20 w-20 lg:hidden bg-gradient-to-br from-orange-400 to-orange-700 items-center justify-center absolute bottom-2 right-2 rounded-full p-2 fixed"
+      className="h-15 w-18 bg-gradient-to-r from-purple-400 to-purple-500 lg:hidden border-t-3 border-l-3 border-b-3 border-zinc-300/10 items-center justify-center absolute bottom-2 right-0 rounded-l-full p-2 fixed animate-pulse"
     >
-      chat
+      Ask AI
     </button>
   );
 }
