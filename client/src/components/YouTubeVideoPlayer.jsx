52dd1c36d9342a7960262dbcfda6ee2567f3dcaa
Prakash Kumar <prakashyadav9576@gmail.com>
2025-12-14 18:32:02 +0530

    add youtube player

---
+++ client/src/components/YouTubeVideoPlayer.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import YouTube from "react-youtube";
+
+function YouTubeVideoPlayer({ videoID }) {
+  function onReadyHandler(event) {
+    // access to player in all event handlers via event.target
+    event.target.playVideo();
+  }
+  const opts = {
+    width: "100%",
+    playerVars: {
+      autoplay: 0,
+      rel: 0,
+      modestbranding: 1,
+    },
+  };
+
+  return (
+    <YouTube
+      videoId={videoID}
+      opts={opts}
+      className="w-full h-full"
+      iframeClassName="w-full h-full"
+    />
+  );
+}
+
+export default YouTubeVideoPlayer;
