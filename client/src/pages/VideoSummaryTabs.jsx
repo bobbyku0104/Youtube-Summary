1030fb19290b781f5ae10a14363d461e79389c9e
Prakash Kumar <prakashyadav9576@gmail.com>
2025-12-14 18:33:28 +0530

    add youtube player component

--- client/src/pages/VideoSummaryTabs.jsx
+++ client/src/pages/VideoSummaryTabs.jsx
@@ -6,6 +6,8 @@ import VideoInfo from "../components/VideoInfo";
 import Content from "../components/contentsymmarysection/Content";
 import ChatBoxButton from "../components/common/ChatBoxButton";
 import { GlobalContext } from "../context/GlobalStates";
+import YouTubeVideoPlayer from "../components/YouTubeVideoPlayer";
+import BgGradient from "../components/common/BgGradient";
 
 export default function VideoSummaryPage() {
   const [activeTab, setActiveTab] = useState("summary");
@@ -27,6 +29,7 @@ export default function VideoSummaryPage() {
         >
           X
         </button>
+        <BgGradient color={"purple"} />
         <ChatBox />
       </div>
 
@@ -37,33 +40,36 @@ export default function VideoSummaryPage() {
       <ChatBoxButton fn={changeChatVisiblity} />
 
       {/* Main Layout */}
-      <div
-        className="grid grid-cols-1 lg:grid-cols-3 gap-6 
-                      px-3 sm:px-6 md:px-10 
-                      max-w-8xl mx-auto w-full"
-      >
-        <div className="md:col-span-2 containerBG border border-zinc-300/10 rounded-xl shadow p-4 sm:p-5">
-          <VideoPreview
-            imageURL={
-              "https://i.ytimg.com/vi/h8-qemIbXbo/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLB6-10MXLHT9rml4ywTLKpNL0kKPw"
-            }
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 px-3 sm:px-6 md:px-10 max-w-8xl mx-auto w-full">
+        {/* bg glow */}
+        <BgGradient />
+
+        <div className="md:col-span-2 containerBG border border-zinc-300/10 rounded-xl shadow p-4 sm:p-5 bg-red-500 z-2">
+          {/* youtube player */}
+          <div className="w-full flex items-center justify-center aspect-video rounded-xl overflow-hidden border border-zinc-300/10">
+            <YouTubeVideoPlayer videoID={"d-_ieXc4K7c"} />
+          </div>
+          <VideoInfo
+            title={`Modern React Development Masterclass
+        Modern React `}
           />
-          <VideoInfo />
         </div>
 
-        <div className="hidden lg:flex w-full">
+        {/* chat box */}
+        <div className="hidden lg:flex w-full relative">
+          <BgGradient />
           <ChatBox />
         </div>
       </div>
 
       {/*  TABS  */}
       <div className="w-full max-w-8xl mx-auto px-3 sm:px-6 md:px-10">
-        <div className="mt-10 flex gap-5">
+        <div className="mt-10 flex items-center justify-between sm:justify-start sm:gap-5">
           {["summary", "transcript", "moments"].map((tab) => (
             <button
               key={tab}
               onClick={() => setActiveTab(tab)}
-              className={`px-4 py-2 rounded-xl transition-all duration-200 ease-in-out
+              className={`px-4 py-2 rounded-xl transition-all duration-200 ease-in-out text-md
                 ${
                   activeTab === tab
                     ? "containerBG text-white border border-purple-500"
