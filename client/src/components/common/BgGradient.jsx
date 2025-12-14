3b2aa32bff64d7173bc639df190385f099eb24a6
Prakash Kumar <prakashyadav9576@gmail.com>
2025-12-14 18:32:43 +0530

    change some css

--- client/src/components/common/BgGradient.jsx
+++ client/src/components/common/BgGradient.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
-function BgGradient() {
-  return <div className="h-106 w-106 blur-[150px] opacity-18 absolute bg-blue-500 rounded-full"></div>;
+function BgGradient({ color }) {
+  const colors = { purple: "bg-purple-500" };
+  return (
+    <div
+      className={`${
+        colors[color] ? colors[color] : " bg-blue-500"
+      } h-101 w-92 blur-[150px] opacity-18 absolute rounded-full`}
+    ></div>
+  );
 }
 
 export default BgGradient;
