e18cdb0bfeed84890c18cf7f3d0e17a5e8de09aa
Prakash Kumar <prakashyadav9576@gmail.com>
2025-12-30 18:27:53 +0530

    add summary route

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import summaryRouter from "./routes/summary.route.js";
 
 const app = express();
 
@@ -7,6 +8,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/summary", summaryRouter);
 
 app.get("/", (req, res) => {
   res.status(500).json({
