76251f5852e7a689f08b847ce11696fc0f683c40
bobbyku0104 <143bobby123@gmail.com>
2026-01-01 20:52:14 +0530

    make summay  api

---
+++ client/src/api/summaryApi.js
@@ -0,0 +1,7 @@
+const base_url = import.meta.env.VITE_BASE_URL;
+export default async function summarizeVideo(videoId) {
+  const url = `${base_url}${videoId}`;
+  const res = await fetch(url);
+  const data = await res.json();
+  return data;
+}
