c7926f1b81f81bc703a55f2ab3b636562f0212c9
Prakash Kumar <prakashyadav9576@gmail.com>
2026-01-02 12:43:29 +0530

    backend base url correction

--- client/src/api/summaryApi.js
+++ client/src/api/summaryApi.js
@@ -1,6 +1,7 @@
 const base_url = import.meta.env.VITE_BASE_URL;
 export default async function summarizeVideo(videoId) {
-  const url = `${base_url}${videoId}`;
+  console.log("fetching summary");
+  const url = `${base_url}/summary/${videoId}`;
   const res = await fetch(url);
   const data = await res.json();
   return data;
