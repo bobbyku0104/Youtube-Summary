f3d68ab7c9bcf7c7d96a04aa524354bdb8db81f6
Prakash Kumar <prakashyadav9576@gmail.com>
2026-01-08 03:42:10 +0530

    connect to redis

--- server/src/app.js
+++ server/src/app.js
@@ -2,10 +2,13 @@ import express from "express";
 import cors from "cors";
 import summaryRouter from "./routes/summary.route.js";
 import chatRoute from "./routes/chat.route.js";
+import { connectToRedis } from "./redis/redisClient.js";
 
 const app = express();
 
 // middlewares
+
+connectToRedis();
 
 app.use(cors());
 app.use(express.json());
