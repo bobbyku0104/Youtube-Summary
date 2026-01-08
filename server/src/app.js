9b6f49bd737b577d453f87d882392c6ed85ecc94
Prakash Kumar <prakashyadav9576@gmail.com>
2026-01-08 20:19:11 +0530

    increase payload size from default to 10mb

--- server/src/app.js
+++ server/src/app.js
@@ -11,7 +11,7 @@ const app = express();
 connectToRedis();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
 
 app.use("/summary", summaryRouter);
 app.use("/ask", chatRoute);
